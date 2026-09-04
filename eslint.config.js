e2f6aadff2fb40dab74a048040c8039638fc0032
shafranek-js <pavel.novicky@gmail.com>
2026-09-04 20:40:42 +0200

    chore: stabilize reproducible CadenceFlow toolchain

--- eslint.config.js
+++ eslint.config.js
@@ -15,6 +15,15 @@ export default tseslint.config(
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "react-hooks/set-state-in-effect": "off",
     },
   },
 );
